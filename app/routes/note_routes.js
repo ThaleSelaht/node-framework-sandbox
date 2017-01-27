955a975d2870ac231624d54b92b4b9bc1ee03df8
Thales <thalespizza@gmail.com>
2017-01-27 03:12:13 -0200

    Get and Post Routes

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,6 +1,23 @@
 module.exports = function(app, db) {
+  app.get('/notes/:id', function(req, res) {
+    const details = { '_id': "<ID GOES HERE>" };
+    db.collection('notes').findOne(details, (err, item) => {
+      if (err) {
+        res.send({'error':'An error has occurred'});
+      } else {
+        res.send(item);
+      }
+    });
+  });
+
   app.post('/notes', (req, res) => {
-    console.log(req.body);
-    res.send('Hello');
+    const note = { text: req.body.body, title: req.body.title};
+    db.collection('notes').insert(note, (err, results) => {
+      if (err) { 
+        res.send({ 'error': 'An error has occurred' }); 
+      } else {
+        res.send(results.ops[0]);
+      }
+    });
   });
 };
